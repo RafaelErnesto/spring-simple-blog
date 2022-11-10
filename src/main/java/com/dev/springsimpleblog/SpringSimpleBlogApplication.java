07cc38d86f30a785c53b3697e03b39f4a23208ca
Rafael <rflernesto@gmail.com>
2022-11-09 20:39:02 -0400

    initial commit

---
+++ src/main/java/com/dev/springsimpleblog/SpringSimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springsimpleblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/springsimpleblog/SpringSimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springsimpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
